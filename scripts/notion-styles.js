48216fa83b86841acdfab743d14529471f8ef39c
Nadya Djojosantoso <hello@nadyafebi.com>
2021-07-20 20:29:58 -0700

    Update Notion Styles script

--- scripts/notion-styles.js
+++ scripts/notion-styles.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Notion Font
+// @name         Notion Styles
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Change your Notion default font.
+// @version      1.1
+// @description  Change your Notion default appearance.
 // @author       Nadya Djojosantoso
 // @match        https://www.notion.so/USERNAME/*
 // @grant        GM_addStyle
@@ -10,8 +10,11 @@
 
 // NOTE: Change the USERNAME part of @match above to your username.
 
-const font = "Source Sans Pro"; // Change this to whatever font you want to use.
+// Settings
+const bodyFont = "Source Sans Pro"; // Change this to whatever font you want to use for texts.
+const headerFont = "Source Serif Pro"; // Change this to whatever font you want to use for headers.
 const boldHeaders = true; // Whether you want the headers to be bold or not.
+const hidePageMentionArrow = true; // Hide the arrow next to a page mention.
 
 GM_addStyle(`
     .notion-frame .notion-selectable,
@@ -19,18 +22,48 @@ GM_addStyle(`
     div[placeholder="Heading 1"],
     div[placeholder="Heading 2"],
     div[placeholder="Heading 3"] {
-        font-family: "${font}" !important;
+        font-family: "${bodyFont}" !important;
     }
 `);
 
-if (boldHeaders)
-{
+if (headerFont) {
+    GM_addStyle(`
+        div[placeholder="Untitled"],
+        div[placeholder="Heading 1"],
+        div[placeholder="Heading 2"],
+        div[placeholder="Heading 3"] {
+            font-family: "${headerFont}" !important;
+        }
+    `);
+}
+
+if (boldHeaders) {
     GM_addStyle(`
         div[placeholder="Untitled"],
         div[placeholder="Heading 1"],
         div[placeholder="Heading 2"],
         div[placeholder="Heading 3"] {
             font-weight: bold !important;
+        }
+    `);
+}
+
+if (hidePageMentionArrow) {
+    GM_addStyle(`
+        .notion-page-mention-token svg {
+            display: none !important;
+        }
+
+        .notion-page-mention-token > span > span {
+            width: auto !important;
+            margin-right: 2px !important;
+            padding-left: 2px !important;
+            vertical-align: text-top;
+        }
+
+        .notion-page-mention-token img {
+            width: 19.8px !important;
+            height: auto !important;
         }
     `);
 }
